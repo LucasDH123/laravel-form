3f88316cafb443bc04d68459f54c886a1b7c3a21
LucasDH123 <75067759+LucasDH123@users.noreply.github.com>
2023-05-15 10:20:20 +0200

    fix

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -53,14 +53,14 @@ class AuthenticationController extends Controller
         }
     }
 
-    public function logout(Request $request){
-    
-    Auth::logout();
- 
-    $request->session()->invalidate();
- 
-    $request->session()->regenerateToken();
- 
-    return redirect()->to('/');
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->to('/');
     }
 }
