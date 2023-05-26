bdec098770b352cfd59c86e1f1dababccd27b710
LucasDH123 <75067759+LucasDH123@users.noreply.github.com>
2023-05-26 09:29:13 +0200

    updated seeder

--- database/seeders/Userseeder.php
+++ database/seeders/Userseeder.php
@@ -17,11 +17,19 @@ class Userseeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 50; $i++) {
+        User::create([
+            'name' => 'Lucas',
+            'email' => 'lucasdh2003@gmail.com',
+            'password' => 'Luc@$@dmin#111',
+            'is_admin' => 1
+        ]);
+
+        for ($i = 1; $i <= 49; $i++) {
             User::create([
                 'name' => Str::random(10),
                 'email' => Str::random(10).'@gmail.com',
-                'password' => "password"
+                'password' => "password",
+                'is_admin' => 0
                 ]);
         }
     }
